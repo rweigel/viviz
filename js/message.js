a0dcc9355574caffa6fa8dc8f41d9717822c4401
Robert Weigel <rweigel@gmu.edu>
2014-07-25 10:16:11 -0400

    Fix for wrapper not defined error

--- js/message.js
+++ js/message.js
@@ -1,24 +1,24 @@
-	function error (msg,clear) {
-		$(wrapper + ' #error').show();
-		if (clear) {
-			$(wrapper + ' #error').html(msg);
-		} else {
-			$(wrapper + ' #error').append(msg)
-		};
-	}
-	
-	function warning (msg,clear,totime) {
-		$(wrapper + ' #warning').show();
-		if (clear) {
-			$(wrapper + ' #warning').html(msg);
-		} else {
-			spacer="";
-			if ($(wrapper + ' #warning').text().length > 0) {
-				spacer=" | ";
-			};
-			$(wrapper + ' #warning').append(spacer + msg);
-		}
+function error (wrapper,msg,clear) {
+	$(wrapper + ' #error').show();
+	if (clear) {
+		$(wrapper + ' #error').html(msg);
+	} else {
+		$(wrapper + ' #error').append(msg)
+	};
+}
 
-		if (totime != Infinity)
-			setTimeout(function () {$(wrapper + ' #warning').html('').hide();},totime || 3000);
+function warning (wrapper,msg,clear,totime) {
+	$(wrapper + ' #warning').show();
+	if (clear) {
+		$(wrapper + ' #warning').html(msg);
+	} else {
+		spacer="";
+		if ($(wrapper + ' #warning').text().length > 0) {
+			spacer=" | ";
+		};
+		$(wrapper + ' #warning').append(spacer + msg);
 	}
+
+	if (totime != Infinity)
+		setTimeout(function () {$(wrapper + ' #warning').html('').hide();},totime || 3000);
+}
